ff4819e69022e5aa6a5c220da6db2c046bedad26
gu-ramos12 <luiz.gustavo.rg1@gmail.com>
2026-04-29 15:25:57 -0300

    Add files via upload

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,33 @@
+import { Header } from "../components/Header";
+import { HeroSection } from "../components/HeroSection";
+import { AboutCoachSection } from "../components/AboutCoachSection";
+import { WhySection } from "../components/WhySection";
+import { MethodSection } from "../components/MethodSection";
+import { ProtocolSection } from "../components/ProtocolSection";
+import { TransformationSection } from "../components/TransformationSection";
+import { OfferSection } from "../components/OfferSection";
+import { FAQSection } from "../components/FAQSection";
+import { FinalCTASection } from "../components/FinalCTASection";
+import { Footer } from "../components/Footer";
+import { StickyCTAMobile } from "../components/StickyCTAMobile";
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main className="pb-16 md:pb-0">
+        <HeroSection />
+        <AboutCoachSection />
+        <WhySection />
+        <MethodSection />
+        <ProtocolSection />
+        <TransformationSection />
+        <OfferSection />
+        <FAQSection />
+        <FinalCTASection />
+      </main>
+      <Footer />
+      <StickyCTAMobile />
+    </div>
+  );
+}
